d7342727bc320c219b29a92006d598b82d9a9ece
George Moore <g.t.moore@lancaster.ac.uk>
2023-11-25 21:28:30 +0000

    feat: Added basic meal plan functionality

--- Scripts/mealplan.service.js
+++ Scripts/mealplan.service.js
@@ -1,17 +1,84 @@
-function addMeal() {
+document.addEventListener('DOMContentLoaded', function () {
+  loadItems();
+});
+
+function addItem() {
+  console.log("addItem triggered.");
+  var mealInput = document.getElementById("mealInput");
+  var mealPlan = document.getElementById("mealPlan");
   const day = document.getElementById('day').value;
-  const meal = document.getElementById('meal').value;
 
-  if (meal.trim() !== '') {
-      const mealPlanList = document.getElementById('mealPlan');
-      const listItem = document.createElement('li');
-      listItem.textContent = `${day.charAt(0).toUpperCase() + day.slice(1)}: ${meal}`;
-      mealPlanList.appendChild(listItem);
+  if (mealInput.value.trim() !== "") {
+    var listItem = document.createElement("li");
 
-      // Clear input fields
-      document.getElementById('day').value = 'monday';
-      document.getElementById('meal').value = '';
-  } else {
-      alert('Please enter a meal before adding to the plan.');
+    listItem.textContent = `${day.charAt(0).toUpperCase() + day.slice(1)}: ${mealInput.value}`;
+
+    var removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.onclick = async function () {
+      PostItemToServer('/removeMealItem', listItem);
+      mealPlan.removeChild(listItem);
+      dbItems = await GetItemsFromServer('/getMealItems')
+      SaveItemsToCache('mealItems', dbItems);
+    };
+    AddItemToCache('mealItems', mealInput.value);
+
+    mealInput.value = "";
+
+    
+    PostItemToServer('/saveMealItem', listItem.textContent);
+    
+
+    listItem.appendChild(removeButton);
+
+    mealPlan.appendChild(listItem);
+  }
+}
+
+async function loadItems() {
+  var mealPlan = document.getElementById("mealPlan");
+
+  var cacheItems = GetItemsFromCache('mealItems');
+  if (cacheItems)
+  {
+    cacheItems.forEach(item => {
+      var listItem = document.createElement("li");
+      listItem.textContent = item.name;
+
+      var removeButton = document.createElement("button");
+      removeButton.textContent = "Remove";
+      removeButton.disabled = true;
+
+      listItem.appendChild(removeButton);
+      mealPlan.appendChild(listItem);
+    });
+  }
+
+  try {
+    var dbItems = await GetItemsFromServer('/getMealItems');
+    if (dbItems) {
+      mealPlan.innerHTML = "";
+      
+      dbItems.forEach(item => {
+        var listItem = document.createElement("li");
+        listItem.textContent = item.name;
+  
+        var removeButton = document.createElement("button");
+        removeButton.textContent = "Remove";
+        removeButton.onclick = async function () {
+          PostItemToServer('/removeMealItem', item.name);
+          mealPlan.removeChild(listItem);
+          dbItems = await GetItemsFromServer('/getMealItems')
+          SaveItemsToCache('mealItems', dbItems);
+        };
+  
+        listItem.appendChild(removeButton);
+        mealPlan.appendChild(listItem);
+      });
+      
+      SaveItemsToCache('mealItems', dbItems);
+    }
+  } catch (error) {
+    console.error('Error loading items from the database:', error);
   }
 }
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 var shoppingListCollection;
+var mealPlanCollection;
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -42,6 +43,16 @@ app.get('/getShoppingListItems', async (req, res) => {
   }
 });
 
+app.get('/getMealItems', async (req, res) => {
+  try {
+    const items = await mealPlanCollection.find({}).toArray();
+    res.json(items);
+  } catch (error) {
+    console.error('Error reading items from MongoDB:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 /**
  * /saveShoppingListItem endpoint.
  * Writes the item to the MongoDB collection called items.
@@ -51,6 +62,18 @@ app.post('/saveShoppingListItem', async (req, res) => {
 
   try {
     await shoppingListCollection.insertOne({ name: item });
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Error writing item to MongoDB:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/saveMealItem', async (req, res) => {
+  const { item } = req.body;
+
+  try {
+    await mealPlanCollection.insertOne({ name: item });
     res.json({ success: true });
   } catch (error) {
     console.error('Error writing item to MongoDB:', error);
@@ -74,11 +97,24 @@ app.post('/removeShoppingListItem', async (req, res) => {
   }
 });
 
+app.post('/removeMealItem', async (req, res) => {
+  const { item } = req.body;
+
+  try {
+    await mealPlanCollection.deleteOne({ name: item });
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Error writing item to MongoDB:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 /**
  * Start up the server on the specified port.
  */
 app.listen(port, '0.0.0.0', async () => {
     shoppingListCollection = await connectToMongo('shopping', 'items');
+    mealPlanCollection = await connectToMongo('mealPlan', 'items');
     console.log(`Server is running at http://${require('os').hostname()}:${port}`);
 });
 
