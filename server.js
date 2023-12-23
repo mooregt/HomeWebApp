9971c1c97531a988e78358bcf0a1879f6fbed327
George Moore <g.t.moore@lancaster.ac.uk>
2023-12-23 19:29:50 +0000

    chore: Removed unnecessary variable declaration

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const { MongoClient } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
-var shoppingListCollection;
-var mealPlanCollection;
-
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
