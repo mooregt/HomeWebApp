f1e97eded3d8db6b1fb90fddda8ce49a252d0d8f
George Moore <g.t.moore@lancaster.ac.uk>
2024-02-13 20:17:12 +0000

    feat: Added clock widget

--- Scripts/common.components.js
+++ Scripts/common.components.js
@@ -39,3 +39,24 @@ function createLabel(labelText) {
   label.id = labelText;
   return label;
 }
+
+function updateClock() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  var timeString = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+  var dateString = now.toDateString();
+
+  document.getElementById('time').innerHTML = timeString;
+  document.getElementById('date').innerHTML = dateString;
+}
+
+updateClock();
