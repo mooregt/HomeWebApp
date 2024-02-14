094604ced2e991d34ab9f19f640358c4184fd990
George Moore <g.t.moore@lancaster.ac.uk>
2024-02-14 19:23:21 +0000

    chore: Minor mealplan and clock improvements

--- Scripts/common.components.js
+++ Scripts/common.components.js
@@ -50,9 +50,8 @@ function updateClock() {
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
   minutes = minutes < 10 ? '0' + minutes : minutes;
-  seconds = seconds < 10 ? '0' + seconds : seconds;
 
-  var timeString = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+  var timeString = hours + ':' + minutes + ' ' + ampm;
   var dateString = now.toDateString();
 
   document.getElementById('time').innerHTML = timeString;
