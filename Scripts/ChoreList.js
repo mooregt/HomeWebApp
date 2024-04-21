2649011db9657b5637005a5f626d4a77d12ea3ee
George Moore <mooretgeorge5@gmail.com>
2024-04-21 18:54:18 +0100

    chore: Renamed script classes

---
+++ Scripts/ChoreList.js
@@ -0,0 +1,163 @@
+// Initialise global constants
+const type = 'chores';
+
+// On page load
+document.addEventListener('DOMContentLoaded', function () {
+  hideLightbox();
+  loadItems();
+});
+
+/**
+ * Adds an item to the chores from the value entered in the itemInput element.
+ */
+function addItem() {
+  var itemInput = document.getElementById("choreInput");
+  const person = document.getElementById('person').value;
+  const frequency = document.getElementById('frequency').value;
+
+  var itemList = document.getElementById("itemList");
+
+  if (itemInput.value.trim() !== "") {
+    var listItem = createListItem(itemInput.value);
+    var itemName = listItem.textContent;
+
+    var assignee = createLabel(person);
+
+    var completeButton = createCompleteButton(async function () {
+      PostItemToServer('/removeItem', type, itemName);
+      PostItemToServer('/saveItem', type, itemName, person, new Date(new Date().setHours(0, 0, 0, 0)), frequency);
+      itemList.removeChild(listItem);
+      dbItems = await GetItemsFromServer(type)
+      SaveItemsToCache(type, dbItems);
+    });
+
+    var removeButton = createRemoveButton(async function () {
+      showLightbox(listItem, itemName);
+    });
+
+    AddItemToCache(type, itemInput.value, person, "1900-01-01T00:00:00.000Z", frequency);
+    itemInput.value = "";
+
+    PostItemToServer('/saveItem', type, listItem.textContent, person, "1900-01-01T00:00:00.000Z", frequency);
+    
+    listItem.appendChild(assignee);
+    listItem.appendChild(completeButton);
+    listItem.appendChild(removeButton);
+    itemList.appendChild(listItem);
+  }
+}
+
+/**
+ * Loads the list of items in the chores collection.
+ */
+async function loadItems()
+{
+  var itemList = document.getElementById("itemList");
+
+  loadFromCache(itemList);
+  await loadFromDatabase(itemList);
+}
+
+/**
+ * Loads the items currently stored in the cache.
+ * @param {HTMLElement} itemList 
+ */
+function loadFromCache(itemList)
+{
+  var cacheItems = GetItemsFromCache(type);
+  if (cacheItems)
+  {
+    cacheItems.forEach(item => {
+      daysSinceLastCompleted = (new Date(new Date().setHours(0, 0, 0, 0)) - new Date(item.lastCompleted)) / (1000 * 60 * 60 * 24);
+
+      if (daysSinceLastCompleted >= item.frequency)
+      {
+        var listItem = createListItem(item.name);
+        var assignee = createLabel(item.person);
+
+        var completeButton = createCompleteButton(null);
+        completeButton.disabled = true;
+        var removeButton = createRemoveButton(null);
+        removeButton.disabled = true;
+
+        listItem.appendChild(assignee);
+        listItem.appendChild(completeButton);
+        listItem.appendChild(removeButton);
+        itemList.appendChild(listItem);
+      }
+    });
+  }
+}
+
+/**
+ * Loads the items currently stored in the database.
+ * @param {HTMLElement} itemList 
+ */
+async function loadFromDatabase(itemList)
+{
+  try {
+    var dbItems = await GetItemsFromServer(type);
+    if (dbItems) {
+      itemList.innerHTML = "";
+      
+      dbItems.forEach(item => {
+        daysSinceLastCompleted = (new Date(new Date().setHours(0, 0, 0, 0)) - new Date(item.lastCompleted)) / (1000 * 60 * 60 * 24);
+
+        if (daysSinceLastCompleted >= item.frequency)
+        {
+          var listItem = createListItem(item.name);
+          var assignee = createLabel(item.person);
+
+          var completeButton = createCompleteButton(async function () {
+            PostItemToServer('/removeItem', type, item.name);
+            PostItemToServer('/saveItem', type, item.name, item.person, new Date(new Date().setHours(0, 0, 0, 0)), item.frequency);
+            itemList.removeChild(listItem);
+            dbItems = await GetItemsFromServer(type)
+            SaveItemsToCache(type, dbItems);
+          });
+
+          var removeButton = createRemoveButton(async function () {
+            showLightbox(listItem, item.name);
+          });
+    
+          listItem.appendChild(assignee);
+          listItem.appendChild(completeButton);
+          listItem.appendChild(removeButton);
+          itemList.appendChild(listItem);
+        }
+      });
+      
+      SaveItemsToCache(type, dbItems);
+    }
+  } catch (error) {
+    console.error('Error loading items from the database:', error);
+  }
+}
+
+// Function to show the lightbox
+function showLightbox(listItem, itemName) {
+  document.getElementById('confirmationLightbox').style.display = 'flex';
+  var prompt = document.getElementById('confirmation-text');
+  prompt.textContent = `You are about to permanently delete "${itemName}". Would you like to continue?`
+
+  // Attach event listeners to buttons
+  document.getElementById('confirmAction').addEventListener('click', function() {confirmCallback(listItem, itemName)});
+  document.getElementById('cancelAction').addEventListener('click', function() {hideLightbox()});
+}
+
+// Function to hide the lightbox
+function hideLightbox() {
+  document.getElementById('confirmationLightbox').style.display = 'none';
+}
+
+async function confirmCallback(listItem, itemName)
+{
+  var itemList = document.getElementById("itemList");
+
+  itemList.removeChild(listItem);
+  PostItemToServer('/removeItem', type, itemName);
+  var dbItems = await GetItemsFromServer(type)
+  SaveItemsToCache(type, dbItems);
+
+  hideLightbox()
+}
---
+++ Scripts/ServerUtilities.js
@@ -0,0 +1,76 @@
+/**
+ * Retrieves an array of items from the server.
+ * @param {string} itemsEndpoint 
+ * @returns {JSON} The items retrieved from the server.
+ */
+async function GetItemsFromServer(type) {
+  try {
+    const response = await fetch('/getItems?type=' + type);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const items = await response.json();
+    
+    return items;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
+
+/**
+ * Posts an item to the server for addition or removal
+ * @param {string} itemsEndpoint 
+ * @param {*} item
+ */
+function PostItemToServer(itemsEndpoint, type, item) {
+  console.log(item);
+  fetch(itemsEndpoint + '?type=' + type, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ item }),
+  })
+    .then(response => response.json())
+    .then(data => console.log('Success:', data))
+    .catch(error => console.error('Error:', error));
+}
+
+/**
+ * Posts two items to the server for addition or removal
+ * @param {string} itemsEndpoint 
+ * @param {*} item
+ */
+function PostItemToServer(itemsEndpoint, type, item, item2) {
+  fetch(itemsEndpoint + '?type=' + type, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ item, item2 }),
+  })
+    .then(response => response.json())
+    .then(data => console.log('Success:', data))
+    .catch(error => console.error('Error:', error));
+}
+
+/**
+ * Posts five items to the server for addition or removal
+ * @param {string} itemsEndpoint 
+ * @param {*} item
+ */
+function PostItemToServer(itemsEndpoint, type, item, item2, item3, item4) {
+  fetch(itemsEndpoint + '?type=' + type, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ item, item2, item3, item4 }),
+  })
+    .then(response => response.json())
+    .then(data => console.log('Success:', data))
+    .catch(error => console.error('Error:', error));
+}
