23d60af4c6f4b056f9e163950b1cbc2f143c18c7
George Moore <mooretgeorge5@gmail.com>
2024-05-06 16:51:16 +0100

    feat: Replaced mealplanner input with dropdown

--- Scripts/MealCreator.js
+++ Scripts/MealCreator.js
@@ -43,7 +43,6 @@ async function loadFromDatabase(itemList)
 {
   try {
     var dbItems = await GetItemsFromServer(type);
-    console.log(dbItems);
     if (dbItems) {
       itemList.innerHTML = "";
       
